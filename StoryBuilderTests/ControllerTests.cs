e1b003eb10808b4273203d27928dd6e1cf88a419
Terry Cox <tcox@svsoft.com>
2021-05-30 08:52:32 -0400

    15 add MSTest unit test project to solution, configure and pass a sample test.

---
+++ StoryBuilderTests/ControllerTests.cs
@@ -0,0 +1,17 @@
+﻿//using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StoryBuilder.Controllers;
+
+namespace StoryBuilderTests
+{
+    [TestClass]
+    public class ControllerTests
+    {
+        [TestMethod]
+        public void TestConstructor()
+        {
+            StoryController controller = new StoryController();
+            Assert.IsNotNull(controller);
+        }
+    }
+}
