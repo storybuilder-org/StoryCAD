f16c8d5db647be8982fc5e399eaa529a05e3ce72
Terry Cox <tcox@svsoft.com>
2021-11-13 18:20:54 -0500

    Correct problem with string converter

--- StoryBuilderLib/Converters/StringToStoryElementConverter.cs
+++ StoryBuilderLib/Converters/StringToStoryElementConverter.cs
@@ -60,10 +60,10 @@ namespace StoryBuilder.Converters
             }
             // Look for the StoryElement corresponding to the passed guid
             // (This is the normal approach)
-            Guid guid = new Guid(value.ToString());
-            if (elements.StoryElementGuids.ContainsKey(guid))
-                return elements.StoryElementGuids[guid];
-            return null;   // Not found
+            if (Guid.TryParse(value.ToString(), out var guid))
+                if (elements.StoryElementGuids.ContainsKey(guid))
+                    return elements.StoryElementGuids[guid];
+            return null;  // Not found
         }
 
         /// <summary>
