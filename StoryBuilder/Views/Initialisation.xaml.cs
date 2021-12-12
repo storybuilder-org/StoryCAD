e2b4b09e132380a9ce1c311dde2165728fbd4d94
Terry Cox <tcox@svsoft.com>
2021-12-12 07:43:29 -0500

    Corrected FolderPicker initialization

--- StoryBuilder/Views/Initialisation.xaml.cs
+++ StoryBuilder/Views/Initialisation.xaml.cs
@@ -1,21 +1,12 @@
 ﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using StoryBuilder.ViewModels;
-using Windows.Storage.Pickers;
+using System;
 using System.Runtime.InteropServices;
 using Windows.Storage;
+using Windows.Storage.Pickers;
+using WinRT;
+
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -43,6 +34,7 @@ namespace StoryBuilder.Views
                 var initializeWithWindow = folderPicker.As<IInitializeWithWindow>();
                 initializeWithWindow.Initialize(hwnd);
             }
+
             folderPicker.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
             folderPicker.FileTypeFilter.Add("*");
             StorageFolder folder = await folderPicker.PickSingleFolderAsync();
