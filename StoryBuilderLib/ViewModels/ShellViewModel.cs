fd45bf898bdc615f7dabaff106cde114502e2325
Terry Cox <tcox@svsoft.com>
2022-03-03 03:47:47 -0500

    Remove irrelevant using statements from ShellViewModel. I am no longer seeing the load / save issues on the Structure tab.

--- StoryBuilderLib/ViewModels/ShellViewModel.cs
+++ StoryBuilderLib/ViewModels/ShellViewModel.cs
@@ -30,9 +30,6 @@ using Windows.Storage.Pickers;
 using StoryBuilder.Services;
 using WinRT;
 using GuidAttribute = System.Runtime.InteropServices.GuidAttribute;
-using System.ComponentModel;
-using Microsoft.UI.Dispatching;
-using Windows.UI.Core;
 
 namespace StoryBuilder.ViewModels
 {
