9fe44ea93018a40b2f90a82634a5824518e6ab31
Jake <49795711+Rarisma@users.noreply.github.com>
2022-04-16 23:17:23 +0100

    Clean

--- StoryBuilderLib/Services/Dialogs/NewProjectPage.xaml.cs
+++ StoryBuilderLib/Services/Dialogs/NewProjectPage.xaml.cs
@@ -2,19 +2,14 @@
 using Microsoft.UI.Xaml.Controls;
 using StoryBuilder.ViewModels;
 using System;
+using System.IO;
 using System.Runtime.InteropServices;
 using Windows.Storage;
 using Windows.Storage.Pickers;
 using WinRT;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
 namespace StoryBuilder.Services.Dialogs;
 
-/// <summary>
-/// An empty page that can be used on its own or navigated to within a Frame.
-/// </summary>
 public sealed partial class NewProjectPage : Page
 {
     public NewProjectPage(UnifiedVM vm)
