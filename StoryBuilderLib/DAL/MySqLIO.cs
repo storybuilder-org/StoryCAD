003bed1fd1039ee7b8daffc30225917237904a1f
Terry Cox <tcox@svsoft.com>
2022-07-03 16:55:22 -0400

    Save work in progress.

---
+++ StoryBuilderLib/DAL/MySqLIO.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Data;
+using System.Runtime.Serialization.Json;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using StoryBuilder.Services.Backend;
+
+namespace StoryBuilder.DAL
+{
+
+    /// <summary>
+    /// don't hold database connection longer than necessary
+    // connection string values from Doppler (with dev and prd variants)
+    // (note: get rid of parse server Doppler data)
+    // logging
+    // async io
+    //    see https://stackoverflow.com/questions/39967208/c-sharp-mysql-driver-async-operations
+    // methods:
+    //   read userid given userid number
+    //   add user (use stored procedure)
+    //   update user (if userid exists and name or email changes)
+    //   add or update preferences (add if add user, update if user exists
+    //   add version
+    /// </summary>
+    public class MySqlIO
+    {
+        private string connectionString = "server=localhost;database=StoryBuilder;uid=stb;pwd=stb;";
+
+        /// <summary>
+        ///  Read a specific users row given the id
+        /// </summary>
+        /// <param name="id"></param>
+        public async Task<UsersTable> ReadUsers(int id) 
+        {
+            string sql = "SELECT id, user_name, email,date_added FROM users WHERE id = @Id";
+            UsersTable users = new UsersTable();
+
+            try
+            {
+                await using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    await using (var cmd = new MySqlCommand(sql, conn))
+                    {
+                        cmd.Parameters.Add("@Id", MySqlDbType.Int32);
+                        cmd.Parameters["@Id"].Value = id;
+                        var reader = await cmd.ExecuteReaderAsync();
+
+                        if (reader.Read())
+                        {
+                            users.Id = reader.GetInt32(0);
+                            users.UserName = reader.GetString(1);
+                            users.Email = reader.GetString(2);
+                            users.DateAdded = reader.GetDateTime(3);
+                        }
+                    }
+                }
+
+                return users;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public MySqlIO()
+        {
+            // obtain connection string data from Doppler?
+        }
+    }
+}
---
+++ StoryBuilderLib/Services/Backend/UsersTable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoryBuilder.Services.Backend
+{
+    public class UsersTable
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public DateTime DateAdded { get; set; }
+    }
+    {
+    }
+}
