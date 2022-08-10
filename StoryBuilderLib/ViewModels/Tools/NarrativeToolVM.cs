f579d0f498b34f841fa41e9290a788b1f51ccf23
Jake <49795711+Rarisma@users.noreply.github.com>
2022-08-10 22:06:37 +0100

    Harden code and improve logging.
    Also comments all the code and adds recursive copy in the Copy() function for Section/folders

--- StoryBuilderLib/ViewModels/Tools/NarrativeToolVM.cs
+++ StoryBuilderLib/ViewModels/Tools/NarrativeToolVM.cs
@@ -24,60 +24,65 @@ public class NarrativeToolVM
         DeleteCommand = new RelayCommand(Delete);
     }
 
+    /// <summary>
+    /// Deletes a node from the tree.
+    /// </summary>
     public void Delete()
     {
         Logger.Log(LogLevel.Trace, "Deleting element");
         try
         {
+            //Check for null and ensure the node is suposed to be deleted first, then proceed with deleting.
             if (ShellVM.CurrentNode == null)
             {
-                Logger.Log(LogLevel.Error, "Current node is null, aborting delete");
+                Logger.Log(LogLevel.Warn, "Current node is null, aborting delete");
                 return;
             }
 
             if (ShellVM.CurrentNode.Type == StoryItemType.TrashCan || ShellVM.CurrentNode.IsRoot)
             {
-                Logger.Log(LogLevel.Error, "Cannot delete this node, was either trash can or a Root");
+                Logger.Log(LogLevel.Warn, "Cannot delete this node, was either trash can or a Root");
                 return;
             }
 
-            //Even though there should only be one copy, just delete any just in case.
+            //Check the root of the narrator view first, then recurse.
             if (ShellVM.StoryModel.NarratorView[0].Children.Contains(ShellVM.CurrentNode))
             {
+                Logger.Log(LogLevel.Info, "Node is not a child of a child, removing it from NarratorView[0]");
                 ShellVM.StoryModel.NarratorView[0].Children.Remove(ShellVM.CurrentNode);
                 ShellVM.StoryModel.NarratorView[1].Children.Add(ShellVM.CurrentNode);
             }
-            foreach (StoryNodeItem child in ShellVM.StoryModel.NarratorView[0].Children) { recurseDelete(ShellVM.CurrentNode, child); }
+            
+            foreach (StoryNodeItem child in ShellVM.StoryModel.NarratorView[0].Children)
+            {
+                Logger.Log(LogLevel.Info, "Node is either a child or not in the tree, recursing tree.");
+                RecurseDelete(ShellVM.CurrentNode, child);
+            }
         }
-        catch
-        {
-            Logger.Log(LogLevel.Error, "Error Deleting node in narrative tree");
-        }
-
+        catch (Exception e) { Logger.LogException(LogLevel.Error,e, "Error Deleting node in NarrativeTool.Delete()"); }
     }
 
     /// <summary>
     /// Recursively deletes from NarratorView.
     /// </summary>
-    private void recurseDelete(StoryNodeItem item, StoryNodeItem Parent)
+    private void RecurseDelete(StoryNodeItem item, StoryNodeItem Parent)
     {
+        Logger.Log(LogLevel.Info, "Starting recursive delete instance");
         try
         {
             if (Parent.Children.Contains(item)) //Checks parent contains child we are looking.
             {
+                Logger.Log(LogLevel.Info, "StoryNodeItem found, deleting it.");
                 Parent.Children.Remove(item); //Deletes child.
                 ShellVM.StoryModel.NarratorView[1].Children.Add(ShellVM.CurrentNode);
             }
             else //If child isn't in parent, recurse again.
             {
-                foreach (StoryNodeItem child in Parent.Children) { recurseDelete(item, child); }
+                Logger.Log(LogLevel.Info, "StoryNodeItem not found, recursing again");
+                foreach (StoryNodeItem child in Parent.Children) { RecurseDelete(item, child); }
             }
         }
-        catch (Exception ex)
-        {
-            Logger.LogException(LogLevel.Error, ex,"Error deleting node in Recursive delete");
-        }
-
+        catch (Exception ex) { Logger.LogException(LogLevel.Error, ex,"Error deleting node in Recursive delete"); }
     } 
 
     /// <summary>
@@ -97,18 +102,29 @@ public class NarrativeToolVM
             }
 
             Logger.Log(LogLevel.Info, $"Node Selected is a {ShellVM.CurrentNode.Type}");
-            if (ShellVM.CurrentNode.Type == StoryItemType.Scene)  //check the node is either a scene OR has children
+            if (ShellVM.CurrentNode.Type == StoryItemType.Scene)  //If its just a scene, add it immediately if not already in.
             {
-                if (!RecursiveCheck(ShellVM.StoryModel.NarratorView[0].Children).Any(StoryNodeItem => StoryNodeItem.Uuid == ShellVM.CurrentNode.Uuid))
+                if (!RecursiveCheck(ShellVM.StoryModel.NarratorView[0].Children).Any(StoryNodeItem => StoryNodeItem.Uuid == ShellVM.CurrentNode.Uuid)) //checks node isn't in the narrator view
                 {
                     _ = new StoryNodeItem((SceneModel)ShellVM.StoryModel.StoryElements.StoryElementGuids[ShellVM.CurrentNode.Uuid], ShellVM.StoryModel.NarratorView[0]);
                     Logger.Log(LogLevel.Info, $"Copied ShellVM.CurrentNode {ShellVM.CurrentNode.Name} ({ShellVM.CurrentNode.Uuid})");
-
                 }
                 else
                 {
                     Logger.Log(LogLevel.Warn, $"Node {ShellVM.CurrentNode.Name} ({ShellVM.CurrentNode.Uuid}) already exists in the NarratorView");
                     Message = "This scene already appears in the narrative view.";
+                }
+            }
+            else if (ShellVM.CurrentNode.Type is StoryItemType.Folder or StoryItemType.Section) //If its a folder then recurse and add all unused scenes to the narrative view.
+            {
+                Logger.Log(LogLevel.Info, $"Item is a folder/section, getting flattened list of all children.");
+                foreach (var item in RecursiveCheck(ShellVM.CurrentNode.Children))
+                {
+                    if (item.Type == StoryItemType.Scene && !RecursiveCheck(ShellVM.StoryModel.NarratorView[0].Children).Any(StoryNodeItem => StoryNodeItem.Uuid == item.Uuid))
+                    {
+                        _ = new StoryNodeItem((SceneModel)ShellVM.StoryModel.StoryElements.StoryElementGuids[item.Uuid], ShellVM.StoryModel.NarratorView[0]);
+                        Logger.Log(LogLevel.Info, $"Copied item {ShellVM.CurrentNode.Name} ({ShellVM.CurrentNode.Uuid})");
+                    }
                 }
             }
             else
@@ -125,6 +141,7 @@ public class NarrativeToolVM
 
     private List<StoryNodeItem> RecursiveCheck(ObservableCollection<StoryNodeItem> List)
     {
+        Logger.Log(LogLevel.Info, "New instance of Recursive check starting.");
         List<StoryNodeItem> NewList = new();
         try
         {
@@ -151,9 +168,10 @@ public class NarrativeToolVM
     /// <summary>
     /// This recursively copies any unused scene in the Explorer view.
     /// </summary>
-    /// <param name="Item"></param>
+    /// <param name="Item">The parent item </param>
     private void RecurseCopyUnused(StoryNodeItem Item)
     {
+        Logger.Log(LogLevel.Trace, $"Recursing through {Item.Name} ({Item.Uuid})");
         try
         {
             if (Item.Type == StoryItemType.Scene) //Check if scene/folder/section, if not then just continue.
@@ -162,6 +180,7 @@ public class NarrativeToolVM
                 if (!RecursiveCheck(ShellVM.StoryModel.NarratorView[0].Children).Any(StoryNodeItem => StoryNodeItem.Uuid == Item.Uuid)) 
                 {
                     //Since the node isn't in the node, then we add it here.
+                    Logger.Log(LogLevel.Trace, $"{Item.Name} ({Item.Uuid}) not found in Narrative view, adding it to the tree");
                     _ = new StoryNodeItem((SceneModel)ShellVM.StoryModel.StoryElements.StoryElementGuids[Item.Uuid], ShellVM.StoryModel.NarratorView[0]);
                 }
             }
