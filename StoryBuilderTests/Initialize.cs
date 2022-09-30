b4e8c8883b14d3ac1c0ffead6fc4205db6229d54
Terry Cox <tcox@svsoft.com>
2022-09-30 09:26:36 -0400

    Correct StoryWriter to save Cast control

--- StoryBuilderTests/Initialize.cs
+++ StoryBuilderTests/Initialize.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.UI.Xaml;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.VisualStudio.TestTools.UnitTesting.AppContainer;
 using Microsoft.Windows.ApplicationModel.DynamicDependency;
+using StoryBuilder.Models;
 
 [assembly: WinUITestTarget(typeof(StoryBuilder.App))]
 
@@ -15,6 +17,10 @@ public class Initialize
         // TODO: Initialize the appropriate version of the Windows App SDK.
         // This is required when testing MSIX apps that are framework-dependent on the Windows App SDK.
         Bootstrap.TryInitialize(0x00010001, out var _);
+
+        // Activate app
+
+        //UITestMethodAttribute.DispatcherQueue = GlobalData.MainWindow.DispatcherQueue;
     }
 
     [AssemblyCleanup]
