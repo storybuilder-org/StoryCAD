aaf3b0478ef80224eaf86abbbfbd21e0524830f5
Terry Cox <tcox@svsoft.com>
2023-02-06 03:41:50 -0500

    Add .env loading, logging, unhandled exception handler

--- StoryBuilderTests/App.xaml.cs
+++ StoryBuilderTests/App.xaml.cs
@@ -1,4 +1,13 @@
-﻿using Microsoft.UI.Xaml;
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using CommunityToolkit.Mvvm.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
 using Microsoft.UI.Xaml.Data;
@@ -7,15 +16,29 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using Microsoft.UI.Xaml.Shapes;
 using Microsoft.VisualStudio.TestTools.UnitTesting.AppContainer;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using StoryBuilder.DAL;
+using StoryBuilder.Models;
+using StoryBuilder.Models.Tools;
+using StoryBuilder.Services;
+using StoryBuilder.Services.Backend;
+using StoryBuilder.Services.Installation;
+using StoryBuilder.Services.Json;
+using StoryBuilder.Services.Logging;
+using StoryBuilder.Services.Navigation;
+using StoryBuilder.Services.Preferences;
+using StoryBuilder.Services.Search;
+using StoryBuilder.ViewModels;
+using StoryBuilder.ViewModels.Tools;
+using dotenv.net.Utilities;
+using dotenv.net;
+using Microsoft.Extensions.Options;
+using Syncfusion.Licensing;
+using Path = System.IO.Path;
+using UnhandledExceptionEventArgs = Microsoft.UI.Xaml.UnhandledExceptionEventArgs;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -27,13 +50,33 @@ namespace StoryBuilderTests
     /// </summary>
     public partial class App : Application
     {
+        private LogService _log;
+
         /// <summary>
         /// Initializes the singleton application object.  This is the first line of authored code
         /// executed, and as such is the logical equivalent of main() or WinMain().
         /// </summary>
         public App()
         {
+            ConfigureIoc();
+
+            string path = Path.Combine(Package.Current.InstalledLocation.Path, ".env");
+            DotEnvOptions options = new(false, new[] { path });
+            try
+            {
+                DotEnv.Load(options);
+
+                //Register Syncfusion license
+                string token = EnvReader.GetStringValue("SYNCFUSION_TOKEN");
+                SyncfusionLicenseProvider.RegisterLicense(token);
+            }
+            catch { GlobalData.ShowDotEnvWarning = true; }
+
             this.InitializeComponent();
+
+            _log = Ioc.Default.GetService<LogService>();
+            //TODO: Does the unhandled exception handler belong in the test project?
+            Current.UnhandledException += OnUnhandledException;
         }
 
         /// <summary>
@@ -43,6 +86,11 @@ namespace StoryBuilderTests
         /// <param name="args">Details about the launch request and process.</param>
         protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
         {
+            _log.Log(LogLevel.Info, "StoryBuilder.App launched");
+
+            string pathMsg = string.Format("Configuration data location = " + GlobalData.RootDirectory);
+            _log.Log(LogLevel.Info, pathMsg);
+
             Microsoft.VisualStudio.TestPlatform.TestExecutor.UnitTestClient.CreateDefaultUI();
 
             Window = new MainWindow();
@@ -55,6 +103,71 @@ namespace StoryBuilderTests
             // Replace back with e.Arguments when https://github.com/microsoft/microsoft-ui-xaml/issues/3368 is fixed
             Microsoft.VisualStudio.TestPlatform.TestExecutor.UnitTestClient.Run(Environment.CommandLine);
         }
+
+        private static void ConfigureIoc()
+        {
+            Ioc.Default.ConfigureServices(
+                new ServiceCollection()
+                    // Register services
+                    .AddSingleton<PreferencesService>()
+                    .AddSingleton<NavigationService>()
+                    .AddSingleton<LogService>()
+                    .AddSingleton<SearchService>()
+                    .AddSingleton<InstallationService>()
+                    .AddSingleton<ControlLoader>()
+                    .AddSingleton<ListLoader>()
+                    .AddSingleton<ToolLoader>()
+                    .AddSingleton<ScrivenerIo>()
+                    .AddSingleton<StoryReader>()
+                    .AddSingleton<StoryWriter>()
+                    .AddSingleton<MySqlIo>()
+                    .AddSingleton<BackupService>()
+                    .AddSingleton<AutoSaveService>()
+                    .AddSingleton<DeletionService>()
+                    .AddSingleton<BackendService>()
+                    // Register ViewModels 
+                    .AddSingleton<ShellViewModel>()
+                    .AddSingleton<OverviewViewModel>()
+                    .AddSingleton<CharacterViewModel>()
+                    .AddSingleton<ProblemViewModel>()
+                    .AddSingleton<SettingViewModel>()
+                    .AddSingleton<SceneViewModel>()
+                    .AddSingleton<FolderViewModel>()
+                    .AddSingleton<WebViewModel>()
+                    .AddSingleton<TrashCanViewModel>()
+                    .AddSingleton<UnifiedVM>()
+                    .AddSingleton<InitVM>()
+                    .AddSingleton<TreeViewSelection>()
+                    // Register ContentDialog ViewModels
+                    .AddSingleton<NewProjectViewModel>()
+                    .AddSingleton<NewRelationshipViewModel>()
+                    .AddSingleton<PrintReportDialogVM>()
+                    .AddSingleton<NarrativeToolVM>()
+                    // Register Tools ViewModels  
+                    .AddSingleton<KeyQuestionsViewModel>()
+                    .AddSingleton<TopicsViewModel>()
+                    .AddSingleton<MasterPlotsViewModel>()
+                    .AddSingleton<StockScenesViewModel>()
+                    .AddSingleton<DramaticSituationsViewModel>()
+                    .AddSingleton<SaveAsViewModel>()
+                    .AddSingleton<PreferencesViewModel>()
+                    .AddSingleton<FlawViewModel>()
+                    .AddSingleton<TraitsViewModel>()
+                    // Complete 
+                    .BuildServiceProvider());
+        }
+
+        private void OnUnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            _log.LogException(LogLevel.Fatal, e.Exception, e.Message);
+            _log.Flush();
+            AbortApp();
+        }
+
+        /// <summary>
+        /// Closes the app
+        /// </summary>
+        private static void AbortApp() { Current.Exit(); }
 
         public static Window Window;
     }
