1fb4d4b23632ae06a7a00f7b3bb8435d6b682b4f
Jake <49795711+Rarisma@users.noreply.github.com>
2023-03-30 20:08:58 +0100

    Fix connection issue

--- StoryBuilder/App.xaml.cs
+++ StoryBuilder/App.xaml.cs
@@ -220,9 +220,10 @@ public partial class App
         else
         {
             //TODO: check elmah is bypassed when logging is disabled by user.
-            if (GlobalData.ElmahLogging)
+            GlobalData.ElmahLogging = await _log.AddElmahTarget();
+            if (GlobalData.ElmahLogging )
             {
-                await _log.AddElmahTarget();
+                
                 _log.Log(LogLevel.Info, "elmah.io log target added");
             }
             else  // can have several reasons (no doppler, or an error adding the target){
