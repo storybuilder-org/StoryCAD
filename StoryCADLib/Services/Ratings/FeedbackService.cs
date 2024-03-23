65658d8be09b105b3530cb6db4e5f77bba7e6082
Jake <49795711+Rarisma@users.noreply.github.com>
2024-03-23 20:52:32 +0000

    Add broken feedback hub code.

---
+++ StoryCADLib/Services/Ratings/FeedbackService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Services.Store.Engagement;
+namespace StoryCAD.Services.Ratings;
+
+public class FeedbackService
+{
+    /// <summary>
+    /// Calling this function will open the Feedback hub.
+    /// </summary>
+    public void OpenFeedbackHub()
+    {
+        if (Microsoft.Services.Store.Engagement.StoreServicesFeedbackLauncher.IsSupported())
+        { 
+        }
+}
+}
