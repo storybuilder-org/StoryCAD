03d54028f6bb79b686c9d3e4603a3b0e047df899
Jake <49795711+Rarisma@users.noreply.github.com>
2024-09-23 19:22:25 +0100

    Test DND

--- StoryCAD/Views/ProblemPage.xaml.cs
+++ StoryCAD/Views/ProblemPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using StoryCAD.ViewModels.Tools;
+﻿using Microsoft.UI.Xaml;
+using StoryCAD.ViewModels.Tools;
 
 namespace StoryCAD.Views;
 
@@ -21,4 +22,13 @@ public sealed partial class ProblemPage : BindablePage
     //    myOption.ShowMode = FlyoutShowMode.Transient;
     //    ConflictCommandBarFlyout.ShowAt(NavigationTree, myOption);
     //}
+    private void DroppedItem(object sender, DragEventArgs e)
+    {
+	    var x = sender;
+    }
+
+    private void UIElement_OnDropCompleted(UIElement sender, DropCompletedEventArgs args)
+    {
+	    throw new NotImplementedException();
+    }
 }
