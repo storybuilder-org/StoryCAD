612c989bf33bcac544ef09bc58426ca9077e1e0b
Jake <49795711+Rarisma@users.noreply.github.com>
2024-11-19 18:55:20 +0000

    Update UI to add new Help page
    Close button doesnt work yet.

--- StoryCAD/Views/Shell.xaml.cs
+++ StoryCAD/Views/Shell.xaml.cs
@@ -88,6 +88,13 @@ public sealed partial class Shell
 			await new Services.Dialogs.Changelog().ShowChangeLog();
         }
 
+        ContentDialog CD = new()
+        {
+			Title = "Need help getting started?",
+			Content = new HelpPage(),
+			PrimaryButtonText = "Close",
+		};
+        await Ioc.Default.GetRequiredService<Windowing>().ShowContentDialog(CD);
         //If StoryCAD was loaded from a .STBX File then instead of showing the Unified menu
         //We will instead load the file instead.
         Logger.Log(LogLevel.Info, $"Filepath to launch {ShellVm.FilePathToLaunch}");
