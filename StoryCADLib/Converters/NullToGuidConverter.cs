00953f46d98bc1a95a93b0e84987e0572a48c749
Terry Cox <5607778+terrycox@users.noreply.github.com>
2025-01-29 09:06:04 -0500

    Added converter to convert from null to Guid for storyelement references.

---
+++ StoryCADLib/Converters/NullToGuidConverter.cs
@@ -0,0 +1,16 @@
+using System;
+using Microsoft.UI.Xaml.Data;
+
+namespace StoryCAD.Converters;
+public class NullToGuidConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        return value ?? Guid.Empty;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        return value is Guid guid && guid == Guid.Empty ? null : value;
+    }
+}
