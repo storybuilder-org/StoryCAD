a2a6c3f3ccac916cd6b9b9689f492403a7a87c3b
Terry Cox <5607778+terrycox@users.noreply.github.com>
2025-07-16 09:35:10 -0400

    Trying another approach to blocking drag to root

---
+++ StoryCADLib/Converters/BooleanNegationConverter.cs
@@ -0,0 +1,19 @@
+﻿// BooleanNegationConverter.cs
+using Microsoft.UI.Xaml.Data;
+
+namespace StoryCAD.Converters
+{
+    /// <summary>Negates a Boolean for XAML bindings.</summary>
+    public sealed class BooleanNegationConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return value is bool b && !b;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return value is bool b && !b;
+        }
+    }
+}
